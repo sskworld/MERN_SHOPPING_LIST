dae18f802272de99f7c3482c842ffd5f6499c818
Suraj Kale <connectsskale@gmail.com>
2020-02-28 22:22:15 +0530

    Updated server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path')
 
 const items = require('./routes/api/Items')
 
@@ -15,6 +16,14 @@ mongoose
     .catch(err=>console.log(err))
 
 app.use('/api/items', items);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const port = process.env.PORT || 5000;
 
